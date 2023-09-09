17c10cda2934257f33b2d4217bbad95e3a12fb27
Cyril Derc <cyril_derc@hotmail.com>
2023-09-09 18:27:46 +0200

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Nav.scss";
+const Nav = () => {
+  return (
+    <div className="nav">
+      <button className="nav__burger"></button>
+      <img src="./images/logo.png" alt="Netflix" />
+    </div>
+  );
+};
+
+export default Nav;
