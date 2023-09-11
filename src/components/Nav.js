c7f4068f995396e6d2a9206b2396bbe14b5fe42b
Cyril Derc <cyril_derc@hotmail.com>
2023-09-11 13:57:36 +0200

    NAV - création de la Navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,43 @@
 import React from "react";
 import "./Nav.scss";
+
 const Nav = () => {
   return (
-    <div className="nav">
-      <button className="nav__burger"></button>
-      <img src="./images/logo.png" alt="Netflix" />
+    <div className="nav nav--black">
+      <button className="nav__burger"> ---</button>
+      <img src="./images/logo.png" className="nav__logo" alt="Netflix" />
+      <nav className="nav__links">
+        <a href="/" className="nav__link">
+          Accueil
+        </a>
+        <a href="/" className="nav__link">
+          Séries
+        </a>
+        <a href="/" className="nav__link">
+          Films
+        </a>
+      </nav>
+      <div className="nav__actions">
+        <a href="/" className="nav__action">
+          search
+        </a>
+        <a href="/" className="nav__action">
+          direct
+        </a>
+        <a href="/" className="nav__action">
+          gift
+        </a>
+        <a href="/" className="nav__action">
+          notif
+        </a>
+        <a href="/" className="nav__action">
+          <img
+            src="./images/Netflix-avatar.png"
+            className="nav__avatar"
+            alt="image_avatar"
+          />
+        </a>
+      </div>
     </div>
   );
 };
