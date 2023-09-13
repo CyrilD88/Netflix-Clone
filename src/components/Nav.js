bc02400d9f0b84bad00fc6895c86ac297d2a8799
Cyril Derc <cyril_derc@hotmail.com>
2023-09-13 10:35:31 +0200

    NAVBARE - Finalisation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,17 +6,32 @@ import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 
 const Nav = () => {
+  // constante pour remplissage en noir de la navbar
   const [navBlack, setNavBlack] = useState(false);
+
+  // affichage du menu
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  // remplissage en noir de la navbar
   const transitionNav = () => {
     window.scrollY > 100 ? setNavBlack(true) : setNavBlack(false);
+  };
+
+  const handleclick = () => {
+    toggleMenu ? setToggleMenu(false) : setToggleMenu(true);
   };
 
   useState(() => {
     document.addEventListener("scroll", transitionNav);
   });
+
   return (
-    <div className={`nav ${navBlack && "nav--black"}`}>
-      <button className="nav__burger">
+    <div
+      className={`nav ${
+        navBlack || toggleMenu ? "nav--black" : "nav--transparent"
+      } ${toggleMenu && "show"}`}
+    >
+      <button className="nav__burger" onClick={handleclick}>
         {" "}
         <MenuIcon />
       </button>
