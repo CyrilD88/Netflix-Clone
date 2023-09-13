ea150194022644aba6cfcd7735c217815ca4ff63
Cyril Derc <cyril_derc@hotmail.com>
2023-09-13 11:22:47 +0200

    FOOTER - Mise en place des icones réseaux sociaux

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,61 @@
 import React from "react";
 import "./Footer.scss";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import YouTubeIcon from "@mui/icons-material/YouTube";
 
 const Footer = () => {
-  return <footer className="footer">toto</footer>;
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__socials">
+          <a href="/" className="footer__link">
+            <FacebookIcon />
+          </a>
+          <a href="/" className="footer__link">
+            <InstagramIcon />
+          </a>
+          <a href="/" className="footer__link">
+            <TwitterIcon />
+          </a>
+          <a href="/" className="footer__link">
+            <YouTubeIcon />
+          </a>
+        </div>
+        <ul className="footer__links">
+          <li className="footer__link">
+            <a href="/">Lien footer</a>
+          </li>
+          <li className="footer__link">
+            <a href="/">Lien footer</a>
+          </li>
+          <li className="footer__link">
+            <a href="/">Lien footer</a>
+          </li>
+          <li className="footer__link">
+            <a href="/">Lien footer</a>
+          </li>
+          <li className="footer__link">
+            <a href="/">Lien footer</a>
+          </li>
+          <li className="footer__link">
+            <a href="/">Lien footer</a>
+          </li>
+          <li className="footer__link">
+            <a href="/">Lien footer</a>
+          </li>
+          <li className="footer__link">
+            <a href="/">Lien footer</a>
+          </li>
+          <li className="footer__link">
+            <a href="/">Lien footer</a>
+          </li>
+        </ul>
+        <div className="footer__copy">Netflix Clone - tous droits réservés</div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
