b5ae8f074320700fba9a86cb099f6b343869b637
Cyril Derc <cyril_derc@hotmail.com>
2023-09-14 16:34:35 +0200

    création des constantes pour les films

---
+++ src/config/Requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "f07b0448e8be81c0bc0d992c33198f7f";
+
+const baseURL = "https://api.themoviedb.org/3";
+
+const requests = {
+  fetchTrending: `${baseURL}/trending/all/week?api_key=${API_KEY}`,
+  fetchNetflixOriginals: `${baseURL}/trending/all/week?api_key=${API_KEY}`,
+  fetchTopRated: `${baseURL}/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `${baseURL}/discover/movie?api_key=${API_KEY}&width_genres=28`,
+  fetchComedyMovies: `${baseURL}/discover/movie?api_key=${API_KEY}&width_genres=35`,
+  fetchHorrorMovies: `${baseURL}/discover/movie?api_key=${API_KEY}&width_genres=27`,
+  fetchRomanceMovies: `${baseURL}/discover/movie?api_key=${API_KEY}&width_genres=10749`,
+  fetchDocumentaries: `${baseURL}/discover/movie?api_key=${API_KEY}&width_genres=99`,
+};
