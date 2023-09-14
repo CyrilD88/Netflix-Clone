c238a56f423bd883dde025ae196155cfd22bed59
Cyril Derc <cyril_derc@hotmail.com>
2023-09-14 18:09:24 +0200

    BANNER - useState et useEffect pour afficher le film

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,15 +7,30 @@ import requests from "../config/Requests";
 import axios from "axios";
 
 const Banner = () => {
+  // constante pour aller chercher le film
+  const [movie, setMovie] = useState([]);
+
+  // useEffect utiliser pour la fonction qui fait apparaitre le film
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchTrending);
+
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+    }
+    fetchData();
+  }, []);
+
   return (
     <header className="banner">
       <div className="banner__content">
-        <h1 className="banner__title">Titre</h1>
-        <p className="banner__description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-          aut suscipit dolorem architecto fugit illo quidem eius quasi, mollitia
-          sed, in nisi modi accusamus? Rem sit enim officia repudiandae unde?
-        </p>
+        <h1 className="banner__title">
+          {movie.title || movie?.original_title}
+        </h1>
+        <p className="banner__description">{movie?.overview}</p>
         <div className="banner__buttons">
           <button className="banner__button banner__button--play">
             <PlayArrowIcon /> Lecture
